4ea379ec6d6b012cdf3e5703178f56064fed2dd1
screamsoda <sdetstvanaam@mail.ru>
2024-09-21 17:01:28 +0300

    feat: button interaction practice

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -7,11 +7,20 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
+  var1:string = "1"
+  var2:string = "2"
+  public games = ["World Of Warcraft", "Persona 5", "Outer Wilds", "Battlefield 1", "Cyberpunk 2077"]
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
   constructor() {}
 
+  btn_click(){
+      this.var1 = "?"
+      this.var2 = "..."
+  }
+
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
   }
+
 }
