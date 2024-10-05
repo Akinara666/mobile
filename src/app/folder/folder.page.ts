e58861414b62c8a417d84585dada3c537ba11f79
taba4ek555 <146178706+taba4ek555@users.noreply.github.com>
2024-10-05 16:05:43 +0300

    feat: adding games, rating to table

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -13,16 +13,11 @@ interface GType{
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
-  var1:string = "1"
-  var2:string = "2"
-  public gameName = '';
-  public games = ["World Of Warcraft", "Persona 5", "Outer Wilds", "Battlefield 1", "Cyberpunk 2077"];  
   public folder!: string;
-  public appPages = inject(AppComponent).appPages;
   private activatedRoute = inject(ActivatedRoute);
 
-  private game_name = null;
-  private game_score = null;
+  game_name = '';
+  game_rating = '';
 
 
   constructor() {}
@@ -30,9 +25,8 @@ export class FolderPage implements OnInit {
   games_list: GType[] = []
 
   btn_click(){
-      this.var1 = "?"
-      this.var2 = "..."
-      this.games.push(this.gameName)
+    let game : GType = {g_name: this.game_name, g_rating: this.game_rating}
+    this.games_list.push(game)
   }
 
   ngOnInit() {
