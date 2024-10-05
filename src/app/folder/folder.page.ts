00e85551ed9d80fdba1eeafbd7ca541fa35a40a5
taba4ek555 <146178706+taba4ek555@users.noreply.github.com>
2024-10-05 15:45:09 +0300

    feat: adding values in list

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AppComponent } from '../app.component';
+
+interface GType{
+  g_name: string
+  g_rating: string
+}
 
 @Component({
   selector: 'app-folder',
@@ -9,14 +15,24 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
   var1:string = "1"
   var2:string = "2"
-  public games = ["World Of Warcraft", "Persona 5", "Outer Wilds", "Battlefield 1", "Cyberpunk 2077"]
+  public gameName = '';
+  public games = ["World Of Warcraft", "Persona 5", "Outer Wilds", "Battlefield 1", "Cyberpunk 2077"];  
   public folder!: string;
+  public appPages = inject(AppComponent).appPages;
   private activatedRoute = inject(ActivatedRoute);
+
+  private game_name = null;
+  private game_score = null;
+
+
   constructor() {}
+
+  games_list: GType[] = []
 
   btn_click(){
       this.var1 = "?"
       this.var2 = "..."
+      this.games.push(this.gameName)
   }
 
   ngOnInit() {
@@ -24,3 +40,4 @@ export class FolderPage implements OnInit {
   }
 
 }
+
